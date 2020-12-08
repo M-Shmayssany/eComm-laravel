d38e33d0cfbec345c7c05456129e72e9ca73eae8
Mohamed <m.shmayssany@gmail.com>
2020-12-08 23:50:05 +0100

    login and session

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,19 +4,20 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4 offset-sm-4">
-                <form>
+                <form action="login" method="POST">
+                    @csrf
                     <i class="mb-4 fa fa-address-card-o" style="font-size:72px;color:red"></i>
                     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
                     <label for="inputEmail" class="visually-hidden">Email address</label>
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                    <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
                     <label for="inputPassword" class="visually-hidden">Password</label>
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                    <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
                     <div class="checkbox mb-3">
                         <label>
                         <input type="checkbox" value="remember-me"> Remember me
                         </label>
                     </div>
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
                 </form>
             </div>
         </div>
