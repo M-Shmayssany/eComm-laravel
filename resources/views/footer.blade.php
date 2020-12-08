0e74dca655ed234d343da88f1b4f7c4cc33f964d
Mohamed <m.shmayssany@gmail.com>
2020-12-08 01:46:01 +0100

    add login page main header and footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3 bg-light fixed-bottom">
+    <div class="container">
+      <span class="text-muted">Place sticky footer content here.</span><p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+    </div>
+  </footer>
